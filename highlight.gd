188330dd4220b069e99ea514275739a8dad76316
ryan-w-c <cavanaghryan1@gmail.com>
2020-12-30 17:29:44 -0800

    select now working

--- highlight.gd
+++ highlight.gd
@@ -1,7 +1,7 @@
 extends TileMap
 
-var last
-var cell
+#var last
+#var cell
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -10,14 +10,15 @@ var cell
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	last = world_to_map(get_global_mouse_position().snapped(Vector2(0, 0)))
+	pass
+#	last = world_to_map(get_global_mouse_position().snapped(Vector2(0, 0)))
 	# set up board
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	cell = world_to_map(get_global_mouse_position().snapped(Vector2(0, 0)))
-	if (cell != last):
-		set_cellv(cell, 0)
-		set_cellv(last, -1)
-	last = cell
+#func _process(delta):
+#	cell = world_to_map(get_global_mouse_position().snapped(Vector2(0, 0)))
+#	if (cell != last):
+#		set_cellv(cell, 0)
+#		set_cellv(last, -1)
+#	last = cell
--- pieces.gd
+++ pieces.gd
@@ -2,42 +2,48 @@ extends TileMap
 
 onready var highlight = get_parent().get_node("highlight")
 var whiteTurn : bool = true
-var selected : bool = false
 var last
 var cell
 var selectedCell
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var emptyCell
+var cellID
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# set up board
-	last = world_to_map(get_global_mouse_position().snapped(Vector2(0, 0)))
-
-
+	# TODO set up board
+	emptyCell = world_to_map(get_global_mouse_position().snapped(Vector2(0, 0)))
+	last = emptyCell
+	selectedCell = emptyCell
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-#	var mousePos = get_global_mouse_position()
-#	var loc = world_to_map(mousePos)
-#	var cell = get_cell(loc.x,loc.y)
-#	set_cell(loc.x,loc.y,1)
 	hover()
 	if (whiteTurn):
-		if (Input.is_action_pressed("on_left_click")):
-			selectedCell = cell
-			highlight.set_cellv(cell, 5)
-
+		select(6, 13)
+	else:
+		select(0, 7)
 
 func hover():
 	cell = world_to_map(get_global_mouse_position().snapped(Vector2(0, 0)))
-	if (cell != last):
-		if (last != selectedCell):
-			print(cell, last, selectedCell)
+	cellID = get_cellv(cell)
+	if (cell != last && last != selectedCell && cell != selectedCell):
+		if (cellID != 13):
 			highlight.set_cellv(cell, 0)
-			highlight.set_cellv(last, -1)
+		highlight.set_cellv(last, -1)
+	if (cell == selectedCell && last != selectedCell):
+		highlight.set_cellv(last, -1)
+	if (last == selectedCell && cell != selectedCell):
+		if (cellID != 13):
+			highlight.set_cellv(cell, 0)
 	last = cell
 
+func select(x, y):
+	if (Input.is_action_just_released("on_left_click")):
+		if (cellID > x && cellID < y):
+			if (cell != selectedCell):
+				highlight.set_cellv(selectedCell, -1)
+				selectedCell = cell
+				highlight.set_cellv(cell, 5)
+			else:
+				highlight.set_cellv(selectedCell, 0)
+				selectedCell = emptyCell
